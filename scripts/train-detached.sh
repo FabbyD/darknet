30eb842b7ee53920e3a0897f611c825e3cb7c628
Fabrice Dugas <fabrice.dugas@lg2.com>
2018-06-08 10:46:33 -0400

    Add training and labelling scripts

--- scripts/train-detached.sh
+++ scripts/train-detached.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DATE=$(date +%m%d%H%M%S)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-screen -d -m -S yolo-$DATE ./train.sh
+screen -dmS yolo-$DATE bash -c "$DIR/train.sh"
 
--- scripts/train.sh
+++ scripts/train.sh
@@ -3,12 +3,12 @@
 DARKNETDIR=$HOME/darknet
 LOGDIR=$HOME/darknet/logs
 TIMESTAMP=$(date +"%s")
-LOGFILE=$LOGDIR/$TIMESTAMP.log
-ERRFILE=$LOGDIR/$TIMESTAMP.err.log
+LOGFILE=$LOGDIR/yolov2.$TIMESTAMP.log
 
 mkdir -p $LOGDIR
 
 cd $DARKNETDIR
 
-./darknet detector train cfg/open-images-window.data cfg/yolov2-tiny-window.cfg weights/darknet19_448.conv.23 >> $LOGFILE 2>> $ERRFILE
+#./darknet detector train cfg/lg2-window.data cfg/yolov2-tiny-window.cfg weights/darknet19_448.conv.23 &> $LOGFILE
 
+./darknet detector train cfg/lg2-window.data cfg/yolov2-window.cfg weights/darknet19_448.conv.23 &> $LOGFILE
